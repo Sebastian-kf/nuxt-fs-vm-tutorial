bd501382d1ca413616ec33702bf0d9146d971707
sebastian <bastiankf@gmail.com>
2024-03-29 10:21:58 +0200

    'first-commit'

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script setup>
+	const { data: ditto, error } = await useFetch('/api/ditto')
+</script>
+<template>
+	<h1>Hello World</h1>
+	<ul v-if="ditto">
+		<li>
+			<img :src="ditto.sprite" />
+		</li>
+		<li>
+			{{ ditto.id }}: {{ ditto.name }}
+		</li>
+	</ul>
+	<div v-else>
+		<p>{{ error }}</p>
+	</div>
+</template>
---
+++ pages/login.vue
@@ -0,0 +1,16 @@
+<script setup>
+	const auth = useCookie('pokemon-auth')
+
+	function authenticate() {
+		auth.value = '1fh3'
+	}
+
+	function logout() {
+		auth.value = ''
+	}
+</script>
+<template>
+	<h1>Login</h1>
+	<button @click="authenticate">Login</button>
+	<button @click="logout">Logout</button>
+</template>
